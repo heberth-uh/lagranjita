6b6966ed1e94a8d49491cb250c141ee6544c7d6c
Brandon-Tzab <brandon.tzab@gmail.com>
2021-07-05 10:07:49 -0500

    cambio de login a primera vista

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,8 +1,8 @@
 @extends('layouts.productosLayout')
 
 @section('content')
-<section class="hero is-success is-fullheight">
-  <div class="hero-body">
+<section class="hero  is-fullheight">
+<div class="hero-body">
 <div class="container ">
 <div class="columns is-centered">
 <div class="column is-5-tablet is-4-desktop is-3-widescreen">
--- routes/web.php
+++ routes/web.php
@@ -16,7 +16,7 @@ use App\Http\Controllers\EmpleadoController;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('/auth/login');
 });
 
 // Rutas para Productos
