fff058c9d140dbdfa29eebea6a9ac66677cd98f0
Brandon-Tzab <brandon.tzab@gmail.com>
2021-07-06 15:22:44 -0500

    cambios en la vista empleado

--- resources/views/Empleado/index.blade.php
+++ resources/views/Empleado/index.blade.php
@@ -9,7 +9,7 @@
     <div class="container is-fullhd my-6">
         <div class="encabezado is-flex is-justify-content-space-between">
             <h1 class="title has-text-info">Empleados</h1>
-            <a class="button is-primary" href="{{ url('/empleado/create') }}">Agregar Empleado</a>
+            <a class="button is-info is-outlined " href="{{ url('/empleado/create') }}">Agregar Empleado</a>
         </div>
         <div class="columns is-multiline m-0 is-2">
 
@@ -17,36 +17,38 @@
                 <div class="column is-4-desktop is-6-tablet">
 
                     <div class="card is-flex is-flex-direction-column">
-                        <div class="card-image">
-                            <figure class="image is-128x128">
-                                <img  class="is-rounded" src="{{asset('storage').'/'. $empleado->Imagen}}" alt="Ilustración del empleado">                             </figure>
+
+                        <div class="card-image p-5">
+                            <figure class="image is-1by1">
+                                <img  class="is-rounded" src="{{asset('storage').'/'. $empleado->Imagen}}" alt="Ilustración del empleado"></figure>
                             </figure>
                         </div>
+
                         <div class="card-content">
                             <div class="media">
                                 <div class="media-content">
-                                    <label class="is-size-5">Nombre:</label>
+                                    <label class="is-size-5 has-text-info">Nombre:</label>
                                     <p class="title is-5">{{ $empleado->Nombre }}</p>
-                                    <label class="is-size-5">Apellido Paterno:</label>
+                                    <label class="is-size-5  has-text-info">Apellido Paterno:</label>
                                     <p class="title is-5">{{ $empleado->ApellidoPaterno }}</p>
-                                    <label class="is-size-5">Apellido Materno:</label>
+                                    <label class="is-size-5  has-text-info">Apellido Materno:</label>
                                     <p class="title is-5">{{ $empleado->ApellidoMaterno }}</p>
-                                    <label class="is-size-5">Correo:</label>
+                                    <label class="is-size-5  has-text-info">Correo:</label>
                                     <p class="title is-5">{{ $empleado->Correo}}</p>
-                                    <label class="is-size-5">Telefono:</label>
+                                    <label class="is-size-5  has-text-info">Telefono:</label>
                                     <p class="title is-5">{{ $empleado->Telefono }}</p>
-                                    <label class="is-size-5">Turno:</label>
+                                    <label class="is-size-5  has-text-info">Turno:</label>
                                     <p class="title is-5">{{ $empleado->Turno }}</p>
                                     
                                 </div>
             
                             </div>
-                            <div class="card-footer has-text-centered">
-                                <a class="button is-warning" href="{{ url('empleado/'. $empleado->id.'/edit')}}">Editar</a>
+                            <div class="card-footer has-text-centered is-flex is-justify-content-space-between">
+                                <a class="button is-info " href="{{ url('empleado/'. $empleado->id.'/edit')}}">Editar</a>
                                 <form action="{{url('/empleado/'.$empleado->id)}}" method="post">
                                     @csrf
                                     {{method_field('DELETE')}}
-                                    <input type="submit" class="button is-danger"
+                                    <input type="submit" class="button is-info is-outlined"
                                     onclick="return confirm('¿Realmente desea borrar este producto?')" value="Eliminar">
                                 </form> 
                             </div>
